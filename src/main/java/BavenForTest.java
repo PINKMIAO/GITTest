eae42c01440828d103b35db6c1d372ce9e65d775
baven <bavenzheng@chancetop.com>
2021-11-11 13:56:27 +0800

    "first commit"

---
+++ src/main/java/BavenForTest.java
@@ -0,0 +1,13 @@
+/**
+ * @author Baven
+ * @date 2021/11/11 13:52
+ */
+public class BavenForTest {
+    public static void main(String[] args) {
+        hello();
+    }
+
+    public static void hello() {
+        System.out.println("hello, git");
+    }
+}
