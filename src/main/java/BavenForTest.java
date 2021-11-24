8f59df765efa601168ec99ad12c6f6e0baeca2e1
baven <bavenzheng@chancetop.com>
2021-11-24 14:21:37 +0800

    "学习lambda"

--- src/main/java/BavenForTest.java
+++ src/main/java/BavenForTest.java
@@ -7,8 +7,7 @@ import java.util.stream.Collectors;
  */
 public class BavenForTest {
     public static void main(String[] args) {
-        hello();
-        lisa(new Student());
+        new BavenForTest().test2();
     }
 
     public static void hello() {
@@ -38,30 +37,30 @@ public class BavenForTest {
         list.forEach(System.out::println);
 
         /**
-        list.stream().map(this::book).collect(Collectors.toList()); // 对象类型相同，只是名称不同的替换
-        大创
-        Optional.ofNullable(responseEntity.getBody()).orElseThrow(() -> new BusinessException("非法访问"));
-        畅拓
-        Optional<LibraryBook> existingBook = bookRepository.selectOne("book_name = ?", request.bookName);
-        if (existingBook.isPresent()) {
-            throw new ConflictException("book already exists, bookName=" + request.bookName);
-        }
-
-        Optional
-        Optional 类是一个可以为null的容器对象。如果值存在则isPresent()方法会返回true，调用get()方法会返回该对象。
-        Optional 是个容器：它可以保存类型T的值，或者仅仅保存null。Optional提供很多有用的方法，这样我们就不用显式进行空值检测。
-        Optional 类的引入很好的解决空指针异常。
-        public static String getGender(Student student) {
-            if(null == student) {
-                return "Unkown";
-            }
-            return student.getGender();
-        }
-        变为
-        public static String getGender(Student student) {
-            return Optional.ofNullable(student).map(u -> u.getGender()).orElse("Unkown");
-        }
-         **/
+         *  list.stream().map(this::book).collect(Collectors.toList()); // 对象类型相同，只是名称不同的替换
+         *  大创
+         *  Optional.ofNullable(responseEntity.getBody()).orElseThrow(() -> new BusinessException("非法访问"));
+         *  畅拓
+         *  Optional<LibraryBook> existingBook = bookRepository.selectOne("book_name = ?", request.bookName);
+         *  if (existingBook.isPresent()) {
+         *      throw new ConflictException("book already exists, bookName=" + request.bookName);
+         *  }
+         *
+         *  Optional
+         *  Optional 类是一个可以为null的容器对象。如果值存在则isPresent()方法会返回true，调用get()方法会返回该对象。
+         *  Optional 是个容器：它可以保存类型T的值，或者仅仅保存null。Optional提供很多有用的方法，这样我们就不用显式进行空值检测。
+         *  Optional 类的引入很好的解决空指针异常。
+         *  public static String getGender(Student student) {
+         *      if(null == student) {
+         *          return "Unkown";
+         *      }
+         *      return student.getGender();
+         *  }
+         *  变为
+         *  public static String getGender(Student student) {
+         *      return Optional.ofNullable(student).map(u -> u.getGender()).orElse("Unkown");
+         *  }
+         */
         // .of 是肯定有值    .ofNullable就不确定方法传进来的到底有没有值
         Optional<Student> aaa = Optional.ofNullable(student);
         System.out.println("optional: " + aaa);
@@ -74,5 +73,22 @@ public class BavenForTest {
         aaa.ifPresent(System.out::println);
         // 直接从容器中取出的一致，前提是Student类中有toString
         System.out.println(aaa.orElseThrow(NullPointerException::new));
+    }
+
+    public void test1() {
+        Student student = new Student();
+        ArrayList arrayList = new ArrayList();
+        String[] strArray = {"AB", "BC", "CD", "AAASF"};
+        Arrays.sort(strArray, String::compareToIgnoreCase);
+    }
+
+    public void test2() {
+        // 这功能作用是赋给 convert 了吗，startsWith为静态方法
+        IConvert<String, String> convert = Something::startsWith;
+        System.out.println(convert.convert("123"));
+
+        Something something = new Something();
+        IMyBaven<String, String> myBaven = something::endWith;
+        System.out.println(myBaven.myBaven("123"));
     }
 }
---
+++ src/main/java/Something.java
@@ -0,0 +1,46 @@
+
+/**
+ * @author Baven
+ * @date 2021/11/23 10:21
+ */
+public class Something {
+    Something() {}
+
+    Something(String something) {
+        System.out.println(something);
+    }
+
+    static String startsWith(String s) {
+        return String.valueOf(s.charAt(0));
+    }
+
+    String endWith(String s) {
+        return String.valueOf(s.charAt(s.length()-1));
+    }
+}
+/**
+ * ::表达式[Class::method] -- lambda表达式的一种简写形式。
+ * 当此表达式中右侧的方法，与要实现方法的格式(包括参数类型与返回值类型)匹配时，可用::表达式替代普通的lambda表达式[() -> {}]。 应用场景：已存在目标接口方法的具体实现，避免重复代码。
+ * 用 '::' 来访问类Something中的方法，必须要用 functional interface 来接收，所以需先定义一个接口
+ * 否则编译错误（The target type of this expression must be a functional interface）
+ */
+
+/**
+ * 做 lambda 学习的接口
+ * @param <F> 来源数据
+ * @param <T> 返回数据
+ */
+@FunctionalInterface
+interface IConvert<F, T> {
+    T convert(F form);
+}
+
+/**
+ * 自己随意创建的函数式接口
+ * @param <F>
+ * @param <T>
+ */
+@FunctionalInterface
+interface IMyBaven<F, T> {
+    T myBaven(F from);
+}
